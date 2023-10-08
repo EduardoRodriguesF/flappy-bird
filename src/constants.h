fbb7982540e55da16a55a0052d82c7cb3e3eb348
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-10-08 10:04:35 -0300

    chore: play around with constant values

--- src/constants.h
+++ src/constants.h
@@ -9,18 +9,18 @@
 #define FPS 60
 #define TARGET_FRAME_TIME (1000 / FPS)
 
-#define GRAVITY_FORCE 1.9f
-#define JUMP_FORCE 0.8f
-#define MAX_VERTICAL_SPEED 2.3f
+#define GRAVITY_FORCE 2.8f
+#define JUMP_FORCE 1.3f
+#define MAX_VERTICAL_SPEED 2.8f
 #define FLOOR_HEIGHT (LOGICAL_SCREEN_HEIGHT - 24)
 
-#define SCENE_SPEED 35.0f
+#define SCENE_SPEED 45.0f
 #define BG_SPEED (SCENE_SPEED / 4.0f)
 
-#define GAP_RADIUS 25
+#define GAP_RADIUS 35
 #define PIPE_WIDTH 52
 
-#define PIPE_TIMEOUT_MS (3 * 1000)
+#define PIPE_TIMEOUT_MS (3.5f * 1000)
 
 #define S_BIRD_UPFLAP "yellowbird-upflap.png"
 #define S_BIRD_MIDFLAP "yellowbird-midflap.png"
